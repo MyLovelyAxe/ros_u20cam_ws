3ceca0e454ff0e9425e4b84ef1242ef6badfe007
Jialei Li <lijialei829@gmail.com>
2026-03-22 21:40:09 +0100

    compress raw images into jpg format

--- src/u20cam_720p/u20cam_720p/compressed_image_publisher.py
+++ src/u20cam_720p/u20cam_720p/compressed_image_publisher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Header
 from cv_bridge import CvBridge
 import cv2
 from .camera import U20Camera
@@ -8,13 +9,22 @@ from .constants import CALIB_PARAM_JSON
 
 class U20CAMLiveStreamImagePublisher(Node):
     def __init__(self):
+
         super().__init__('u20cam_live_stream_image_publisher')
-        self.publisher_ = self.create_publisher(Image, '/camera/undistorted_image', 10)
+
+        # Setup publisher for sending compressed images from camera
+        self.publisher_ = self.create_publisher(
+            CompressedImage,
+            "/camera/image_raw/compressed",
+            10,
+        )
         self.timer = self.create_timer(0.05, self.timer_callback)  # 20Hz
         self.u20cam = U20Camera.create_from_json(CALIB_PARAM_JSON)
         self.bridge = CvBridge()
         self.count = 0
+        self.jpeg_quality = 95 # for compressed image
 
+        # Confirm if the camera is opened correctly
         if not self.u20cam.camera_connected:
             self.get_logger().error('Could not open u20cam.')
             rclpy.shutdown()
@@ -24,18 +34,31 @@ class U20CAMLiveStreamImagePublisher(Node):
 
     def timer_callback(self):
         
-        frame = self.u20cam.get_frame()
-        if frame is None:
+        cv2_bgr_frame = self.u20cam.get_frame()
+        if cv2_bgr_frame is None:
             self.get_logger().warning("No frame is returned")
             return
         else:
             if self.count % 100 == 0:
-                self.get_logger().info(f"The {self.count}th frame shape: {frame.shape}")
+                self.get_logger().info(f"The {self.count}th frame shape: {cv2_bgr_frame.shape}")
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # ori_square_img = cvt_raw_image_squre(raw_image=rgb_frame)
-        img_msg = self.bridge.cv2_to_imgmsg(rgb_frame, encoding='rgb8')
-        self.publisher_.publish(img_msg)
+        # Encode to JPEG
+        ok, buf = cv2.imencode(
+            ext=".jpg", 
+            img=cv2_bgr_frame,
+            params=[int(cv2.IMWRITE_JPEG_QUALITY), self.jpeg_quality],
+        )
+        if not ok:
+            self.get_logger().warn("Failed to encode image")
+            return
+        
+        comp = CompressedImage()
+        comp.header = Header()
+        comp.header.stamp = self.get_clock().now().to_msg()
+        comp.header.frame_id = "u20cam_comp_img_frame"
+        comp.format = "jpeg"
+        comp.data = buf.tobytes()
+        self.publisher_.publish(comp)
         self.count += 1
 
 
